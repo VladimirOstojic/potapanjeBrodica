35b7cf3db0a4199524c0a0c86f6aacd07589cb50
igornjagul <igor.njagul@gmail.com>
2019-06-10 08:49:10 +0200

    Konacni commit

--- I2C_Connection_Test/src/rtl/top_i2c_connection_test.vhd
+++ I2C_Connection_Test/src/rtl/top_i2c_connection_test.vhd
@@ -7,9 +7,9 @@ use ieee.std_logic_1164.all;
 entity top_i2c_connection_test is
 	port(
 		i_sw     		: in  std_logic_vector(1 downto 0);
-		o_i2c_scl		: out  std_logic;
+		io_i2c_scl		: inout  std_logic_vector(1 downto 0);
 		o_led    		: out std_logic_vector(1 downto 0);
-		o_i2c_sda    	: out std_logic
+		io_i2c_sda    	: inout std_logic_vector(1 downto 0)
 	);
 end entity top_i2c_connection_test;
 
@@ -20,19 +20,19 @@ architecture arch_hamming_top of top_i2c_connection_test is
 begin
 	-- Encoder instance.
 	
+
+	io_i2c_scl(0) <= 'Z' when i_sw(0) = '1' else '0';
+	io_i2c_sda(0) <= 'Z' when i_sw(1) = '1' else '0';
 	
-	o_led(0) <= '1' when i_sw(0)='1' else '0';
-	o_led(1) <= '1' when i_sw(1)='1' else '0';
-	
-	o_i2c_scl <= '1' when i_sw(0) = '1' else '0';
-	o_i2c_sda <= '1' when i_sw(0) = '1' else '0';
-	
-	
+	io_i2c_scl(1) <= 'Z' when i_sw(0) = '1' else '0';
+	io_i2c_sda(1) <= 'Z' when i_sw(1) = '1' else '0';
 	
 	
-	
-	
+	o_led(0) <= '1' when io_i2c_scl(0) = 'Z' and io_i2c_scl(1) = 'Z' else '0';
+	o_led(1) <= '1' when io_i2c_sda(0) = 'Z' and io_i2c_sda(1) = 'Z' else '0';
 	-- Decoder instance.
+	
+	
 
 	
 end architecture arch_hamming_top;
